d64093f81be01d8e3f193de483f95f59a3c4ab56
serenity smith <serenitysmith24@gmail.com>
2023-01-25 21:43:46 -0500

    added edit button

--- index.js
+++ index.js
@@ -11,7 +11,11 @@ button.addEventListener("click", function (e) {
 
 
     var newDiv = document.createElement("div")
-    newDiv.appendChild(document.createTextNode(myList.value))
+    var newP = document.createElement('p');
+    var newPtext = document.createTextNode(myList.value)
+    newP.appendChild(newPtext)
+    newDiv.appendChild(newP);
+    // newDiv.appendChild(document.createTextNode(myList.value))
     var newEditButton = document.createElement("button")
     newEditButton.appendChild(document.createTextNode("edit"))
     var newButton = document.createElement("button")
@@ -21,14 +25,44 @@ button.addEventListener("click", function (e) {
     li.appendChild(newButton)
     ul.appendChild(li);
 
-    newButton.addEventListener('click', function (e){
+    newButton.addEventListener('click', function (e) {
         //alert("hi")
-         var liItem = e.currentTarget.parentNode
-         liItem.style.display = "none"
-        
+        console.log("hello")
+        var liItem = e.currentTarget.parentNode
+        liItem.style.display = "none"
     })
 
+    newEditButton.addEventListener("click", function (e) {
+
+        // remove text node from div
+
+        var oldText = li.children[0].children[0].textContent
+        li.children[0].children[0].innerText = "";
+
+        var newInput = document.createElement('input');
+        newInput.focus();
+        newInput.value = oldText;
+        li.children[0].appendChild(newInput);
+        //localStorage.setItem("list", "newInput");
+
+
+
+
+        // li.children[0].children[0].removeChild(li.children[0].children[0].firstElementChild)
+
+        // create input element
+
+
+        
+
+        // append input element to list item
+    });
+
+
+
 })
+
+
 
 
 
